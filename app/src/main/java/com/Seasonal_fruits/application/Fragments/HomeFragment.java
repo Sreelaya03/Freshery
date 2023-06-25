70079223729e648cec9364cd5c04b8edcafa7f3e
Tek N. Chaudhary <tekchau21@gmail.com>
2023-06-25 17:47:15 +0530

    Horizontal Scrollbar for different Season

--- app/src/main/java/com/Seasonal_fruits/application/Fragments/HomeFragment.java
+++ app/src/main/java/com/Seasonal_fruits/application/Fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     ArrayList<MainModel> list;
+    TextView tv_spring,tv_summer,tv_winter,tv_autumn;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,21 +43,57 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        dataInitialized();
+        tv_spring=view.findViewById(R.id.spring);
+        tv_summer=view.findViewById(R.id.summer);
+        tv_autumn=view.findViewById(R.id.autumn);
+        tv_winter=view.findViewById(R.id.winter);
+        spring();
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
         MainAdapter mainAdapter=new MainAdapter(list, getContext());
         recyclerView.setAdapter(mainAdapter);
+
+        tv_spring.setOnClickListener(v->{
+            spring();
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setHasFixedSize(true);
+            MainAdapter springAdapter=new MainAdapter(list, getContext());
+            recyclerView.setAdapter(springAdapter);
+        });
+        tv_summer.setOnClickListener(v->{
+            summer();
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setHasFixedSize(true);
+            MainAdapter summerAdapter=new MainAdapter(list, getContext());
+            recyclerView.setAdapter(summerAdapter);
+        });
+        tv_autumn.setOnClickListener(v->{
+            autumn();
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setHasFixedSize(true);
+            MainAdapter autumnAdapter=new MainAdapter(list, getContext());
+            recyclerView.setAdapter(autumnAdapter);
+        });
+        tv_winter.setOnClickListener(v->{
+            winter();
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setHasFixedSize(true);
+            MainAdapter winterAdapter=new MainAdapter(list, getContext());
+            recyclerView.setAdapter(winterAdapter);
+        });
+
 //        mainAdapter.notifyDataSetChanged();
     }
 
-    private void dataInitialized() {
+
+    private void winter() {
         list =new ArrayList<>();
-        list.add(new MainModel(R.drawable.northern_meal,"Northern Meal","40","Northern Indian cuisine is characterized by the use of rich and creamy gravies, flatbread like naan and roti, and a variety of vegetarian dishes."));
+        list.add(new MainModel(R.drawable.northern_meal,"Winter","40","Northern Indian cuisine is characterized by the use of rich and creamy gravies, flatbread like naan and roti, and a variety of vegetarian dishes."));
         list.add(new MainModel(R.drawable.dosa,"Dosa","40","Dosa is a popular South Indian dish made from a fermented batter of rice and lentils, typically served with sambar and chutney."));
         list.add(new MainModel(R.drawable.idly,"Idly","30","Idly is a traditional South Indian breakfast dish made from fermented rice and lentil batter, typically served with sambar and coconut chutney."));
         list.add(new MainModel(R.drawable.pokora,"Pakora","20","Pakora is a popular Indian snack made by deep-frying batter-coated vegetables, paneer, or meat, often served with chutney."));
@@ -63,4 +101,39 @@ public class HomeFragment extends Fragment {
         list.add(new MainModel(R.drawable.veg_pulau,"Veg Pulau","60","Veg pulao is a fragrant Indian rice dish made with basmati rice, mixed vegetables, and aromatic spices, typically served with raita or curry."));
 
     }
+
+    private void autumn() {
+        list =new ArrayList<>();
+        list.add(new MainModel(R.drawable.northern_meal,"Autumn","40","Northern Indian cuisine is characterized by the use of rich and creamy gravies, flatbread like naan and roti, and a variety of vegetarian dishes."));
+        list.add(new MainModel(R.drawable.dosa,"Dosa","40","Dosa is a popular South Indian dish made from a fermented batter of rice and lentils, typically served with sambar and chutney."));
+        list.add(new MainModel(R.drawable.idly,"Idly","30","Idly is a traditional South Indian breakfast dish made from fermented rice and lentil batter, typically served with sambar and coconut chutney."));
+        list.add(new MainModel(R.drawable.pokora,"Pakora","20","Pakora is a popular Indian snack made by deep-frying batter-coated vegetables, paneer, or meat, often served with chutney."));
+        list.add(new MainModel(R.drawable.shrimp_curry,"Shrimp Curry","100","Shrimp curry is a flavorful Indian dish made with succulent shrimp cooked in a spicy and aromatic sauce typically served with rice or bread."));
+        list.add(new MainModel(R.drawable.veg_pulau,"Veg Pulau","60","Veg pulao is a fragrant Indian rice dish made with basmati rice, mixed vegetables, and aromatic spices, typically served with raita or curry."));
+
+
+    }
+
+    private void summer() {
+        list =new ArrayList<>();
+        list.add(new MainModel(R.drawable.northern_meal,"Summer","40","Northern Indian cuisine is characterized by the use of rich and creamy gravies, flatbread like naan and roti, and a variety of vegetarian dishes."));
+        list.add(new MainModel(R.drawable.dosa,"Dosa","40","Dosa is a popular South Indian dish made from a fermented batter of rice and lentils, typically served with sambar and chutney."));
+        list.add(new MainModel(R.drawable.idly,"Idly","30","Idly is a traditional South Indian breakfast dish made from fermented rice and lentil batter, typically served with sambar and coconut chutney."));
+        list.add(new MainModel(R.drawable.pokora,"Pakora","20","Pakora is a popular Indian snack made by deep-frying batter-coated vegetables, paneer, or meat, often served with chutney."));
+        list.add(new MainModel(R.drawable.shrimp_curry,"Shrimp Curry","100","Shrimp curry is a flavorful Indian dish made with succulent shrimp cooked in a spicy and aromatic sauce typically served with rice or bread."));
+        list.add(new MainModel(R.drawable.veg_pulau,"Veg Pulau","60","Veg pulao is a fragrant Indian rice dish made with basmati rice, mixed vegetables, and aromatic spices, typically served with raita or curry."));
+
+    }
+
+    private void spring() {
+        list =new ArrayList<>();
+        list.add(new MainModel(R.drawable.northern_meal,"Spring","40","Northern Indian cuisine is characterized by the use of rich and creamy gravies, flatbread like naan and roti, and a variety of vegetarian dishes."));
+        list.add(new MainModel(R.drawable.dosa,"Dosa","40","Dosa is a popular South Indian dish made from a fermented batter of rice and lentils, typically served with sambar and chutney."));
+        list.add(new MainModel(R.drawable.idly,"Idly","30","Idly is a traditional South Indian breakfast dish made from fermented rice and lentil batter, typically served with sambar and coconut chutney."));
+        list.add(new MainModel(R.drawable.pokora,"Pakora","20","Pakora is a popular Indian snack made by deep-frying batter-coated vegetables, paneer, or meat, often served with chutney."));
+        list.add(new MainModel(R.drawable.shrimp_curry,"Shrimp Curry","100","Shrimp curry is a flavorful Indian dish made with succulent shrimp cooked in a spicy and aromatic sauce typically served with rice or bread."));
+        list.add(new MainModel(R.drawable.veg_pulau,"Veg Pulau","60","Veg pulao is a fragrant Indian rice dish made with basmati rice, mixed vegetables, and aromatic spices, typically served with raita or curry."));
+
+    }
+
 }
